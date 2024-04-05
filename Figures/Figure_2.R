c8579f5923162e42ed2a4e6c00060e79576f1705
The-HQQ <97820029+The-HQQ@users.noreply.github.com>
2024-04-04 23:27:28 -0400

    Update Figure_2.R

--- Figures/Figure_2.R
+++ Figures/Figure_2.R
@@ -1,1 +1,23 @@
+# Clear workspace
+rm(list = ls())
+
+# Load packages
+library(Seurat)
+library(tidyverse)
+library(viridis)
+
+# Load stromal dataset & palette
+str_fetal<-readRDS('stromal_fetal_lung.rds')
+str_palette<-readRDS('stromal_palette.rds')
+
+# Fig 2A
+
+DimPlot(str_fetal, cols = str_palette)
+
+# Fig 2B
+
+
+
+
+
 
