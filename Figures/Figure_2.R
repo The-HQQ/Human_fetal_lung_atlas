447218340f5a013928eed654cf50d6e75c40e342
The-HQQ <97820029+The-HQQ@users.noreply.github.com>
2024-04-04 23:30:00 -0400

    Update Figure_2.R

--- Figures/Figure_2.R
+++ Figures/Figure_2.R
@@ -16,6 +16,20 @@ DimPlot(str_fetal, cols = str_palette)
 
 # Fig 2B
 
+pt <- table(str_fetal$cell_type, str_fetal$sample_week)
+pt <- as.data.frame(pt)
+ggplot(pt, aes(x = Var2, y = Freq, fill = Var1)) +
+    theme_bw(base_size = 15) +
+    geom_col(colour = "black", position = "fill") +
+    xlab("Sample") +
+    ylab("Proportion") +
+    theme(legend.title = element_blank())+
+    theme(axis.text.x = element_text(angle = 90)) +
+    theme(axis.text = element_text(size=25), axis.title = element_text(size=25)) +
+    theme(legend.text=element_text(size=20)) +
+    NoLegend()
+ggsave("~/Fig_1E.pdf", width = 15, height = 9)
+
 
 
 
