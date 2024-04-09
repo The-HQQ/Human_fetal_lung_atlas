723d904c844e3bb1b39f02a5281c4d08e8bf2070
The-HQQ <97820029+The-HQQ@users.noreply.github.com>
2024-04-08 22:19:25 -0400

    Update Supplementary_Figure_4.R

--- Supplementary_Figures/Supplementary_Figure_4.R
+++ Supplementary_Figures/Supplementary_Figure_4.R
@@ -1,5 +1,14 @@
+# Clear workspace
+rm(list = ls())
 
+# Load packages
+library(Seurat)
+library(tidyverse)
+library(viridis)
 
+# Load stromal dataset & palette
+epi_fetal<-readRDS('epi_fetal_lung.rds')
+epi_palette<-readRDS('epi_palette.rds')
 
 # Supp Fig. 4A
 
@@ -14,5 +23,5 @@ ggplot(pt, aes(x = Var2, y = Freq, fill = Var1)) +
     theme(axis.text.x = element_text(angle = 90)) +
     theme(axis.text = element_text(size=25), axis.title = element_text(size=25)) +
     theme(legend.text=element_text(size=20)) +
-    scale_fill_manual(values = c2_palette) +
+    scale_fill_manual(values = epi_palette) +
     NoLegend()
