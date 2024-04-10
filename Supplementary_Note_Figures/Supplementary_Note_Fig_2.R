70ee162c8cb7ce334eb19d0e4c96a54389c19290
The-HQQ <97820029+The-HQQ@users.noreply.github.com>
2024-04-10 14:54:09 -0400

    Update Supplementary_Note_Fig_2.R

--- Supplementary_Note_Figures/Supplementary_Note_Fig_2.R
+++ Supplementary_Note_Figures/Supplementary_Note_Fig_2.R
@@ -98,6 +98,8 @@ xenium.gw18 <-readRDS('data/xenium_gw18_processed.rds')
 xenium.gw15_palette<-readRDS('palettes/xenium_gw15_palette.rds')
 xenium.gw18_palette<-readRDS('palettes/xenium_gw18_palette.rds')
 
+options(future.globals.maxSize = 3000 * 1024^2)
+
 # Select coordinates to zoom
 
 GW15_coords8 <- Crop(xenium.gw15[["fov"]], x = c(12400, 12700), y = c(6580, 6880), coords = "plot")
