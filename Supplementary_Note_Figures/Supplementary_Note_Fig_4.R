661470b0f91cf76ccaf8baeeceedbcc962ba8471
The-HQQ <97820029+The-HQQ@users.noreply.github.com>
2024-04-10 12:39:29 -0400

    Update Supplementary_Note_Fig_4.R

--- Supplementary_Note_Figures/Supplementary_Note_Fig_4.R
+++ Supplementary_Note_Figures/Supplementary_Note_Fig_4.R
@@ -7,5 +7,9 @@ library(tidyverse)
 library(viridis)
 
 # Load visium objects
+vis.gw18<-readRDS('/data/vis.gw18.rds')
+vis.gw15<-readRDS('/data/vis.gw15.rds')
 
+# Define features to plot
 
+features<-
