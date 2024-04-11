d4a49e93350e23cc14edefbed55da88b804d075c
The-HQQ <97820029+The-HQQ@users.noreply.github.com>
2024-04-11 12:32:11 -0400

    Update Supplementary_Note_Fig_3.R

--- Supplementary_Note_Figures/Supplementary_Note_Fig_3.R
+++ Supplementary_Note_Figures/Supplementary_Note_Fig_3.R
@@ -57,7 +57,7 @@ immune_fetal<-ScaleData(immune_fetal, ordered_immune_features)
 
 # Generate heatmap
 
-DoHeatmap(immune_fetal, assay = 'RNA', features = ordered_immune_features, size = 4, angle = 0, group.colors = immune_palette, label = T, group.by = 'num_ident') +
+DoHeatmap(immune_fetal, assay = 'RNA', features = ordered_immune_features, size = 4, angle = 0, group.colors = immune_palette, label = T) +
 scale_fill_viridis(option = "D") + guides(color = "none")+ theme(axis.title = element_text(size=30)) +theme(axis.text.y = element_text(size = 30))
 ggsave("Supplementary_Note_Fig_3C.pdf", width = 40, height = 18)
 
