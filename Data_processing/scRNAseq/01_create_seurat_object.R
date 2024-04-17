ea117905c783b59b224c3f0d4ce777be6347a9fa
The-HQQ <97820029+The-HQQ@users.noreply.github.com>
2024-04-17 11:23:38 -0400

    Update 01_create_seurat_object.R

--- Data_processing/scRNAseq/01_create_seurat_object.R
+++ Data_processing/scRNAseq/01_create_seurat_object.R
@@ -1,13 +1,10 @@
-library(dplyr)
+# Clear workspace
 library(Seurat)
-library(patchwork)
 
 rm(list = ls())
-#path = "/WongProject/yard/run_cellranger_count/"
 ## Insert path for each cellranger count output directory
 data <- Read10X(data.dir = path)
 # Initialize the Seurat object with the raw (non-normalized data).
-### GW_10_2 has 5937 single cells, 17271 genes/features
 data <- CreateSeuratObject(counts = data, project = "", min.cells = 3, min.features = 200)
 data
 
@@ -15,3 +12,5 @@ data
 data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^MT-")
 
 saveRDS(data, file = 'GW_10_2.rds')
+
+# Repeat above steps with each sample
